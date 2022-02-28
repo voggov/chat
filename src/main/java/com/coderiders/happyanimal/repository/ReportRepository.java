4f86a12682f15c4e16b09b1bd3b2dd1f0532e274
Alexander Shashkov <alexsh2307@mail.ru>
2022-02-28 14:16:16 +0300

    Попроавил маппер

--- src/main/java/com/coderiders/happyanimal/repository/ReportRepository.java
+++ src/main/java/com/coderiders/happyanimal/repository/ReportRepository.java
@@ -2,7 +2,6 @@ package com.coderiders.happyanimal.repository;
 
 import com.coderiders.happyanimal.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
