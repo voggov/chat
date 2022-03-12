647da5ecf2e19b13eecefb9f8ca20eafbe412876
Alexander Shashkov <alexsh2307@mail.ru>
2022-03-12 20:41:11 +0300

    HttpStatus у NotFoundException - NOT_FOUND

--- src/main/java/com/coderiders/happyanimal/exceptions/NotFoundException.java
+++ src/main/java/com/coderiders/happyanimal/exceptions/NotFoundException.java
@@ -3,7 +3,7 @@ package com.coderiders.happyanimal.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
