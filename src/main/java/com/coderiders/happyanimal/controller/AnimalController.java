8a207a58757e51949e852afecdf5343b4c6cf71a
Alexander Shashkov <alexsh2307@mail.ru>
2022-03-14 14:27:01 +0300

    Небольшой рефакторинг

--- src/main/java/com/coderiders/happyanimal/controller/AnimalController.java
+++ src/main/java/com/coderiders/happyanimal/controller/AnimalController.java
@@ -6,7 +6,6 @@ import com.coderiders.happyanimal.service.AnimalService;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/coderiders/happyanimal/controller/handler/HandlerException.java
+++ src/main/java/com/coderiders/happyanimal/controller/handler/HandlerException.java
@@ -5,16 +5,10 @@ import com.coderiders.happyanimal.exceptions.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class HandlerException extends ResponseEntityExceptionHandler {
